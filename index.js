a59bfddd2f7302cbc9f247d35bd437f848770aba
SimranSharma-ui <sharmasimran0675@gmail.com>
2025-03-18 19:54:20 +0530

    fix bug

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(
   cors({
     origin: [
       "https://blog-app-frontend-peach.vercel.app",
-      "https://blog-app-frontend-git-main-simrans-projects-dee52ad7.vercel.app/", // ✅ Allow Vercel frontend
-      "http://localhost:5173", // ✅ Allow local development
+      "https://blog-app-frontend-git-main-simrans-projects-dee52ad7.vercel.app/", 
+      "http://localhost:5173", 
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
